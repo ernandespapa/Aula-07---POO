909ce634e6aec4c3bea89800fb736a82037e74ad
ernandes <ernandes@ernandes-PC>
2019-09-10 14:44:00 -0300

    Db.Java

--- Aula07/src/java/br/com/fatecpg/contatos/Db.java
+++ Aula07/src/java/br/com/fatecpg/contatos/Db.java
@@ -5,10 +5,33 @@
  */
 package br.com.fatecpg.contatos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ernandes
  */
 public class Db {
+    private static ArrayList<Contato> contatos;
+    public static ArrayList<Contato> getContatos(){
+        if(contatos == null){
+            contatos= new ArrayList<>();
+            contatos.add(new Contato("Fulano", "fulano@domain.com", "(13) 99991-0001"));
+            contatos.add(new Contato("Beltrano", "beltrano@domain.com", "(13) 99991-0002"));
+            contatos.add(new Contato("Cicrano", "cicrano@domain.com", "(13) 99991-0003"));
+        }
+        return contatos;    
+    }
+    public static void addContato (Contato contato){
+        contatos.add(contato);
+    }
     
+    public static void updateContato (int index, Contato contato){
+        contatos.set(index, contato);
+    }
+    
+     public static void updateContato (int index){
+        contatos.remove(index);
+    }
+            
 }
